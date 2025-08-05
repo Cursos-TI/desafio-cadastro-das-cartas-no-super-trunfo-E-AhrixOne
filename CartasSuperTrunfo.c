89aa009a6bef12dc217ed61228f24371d6554940
Eliel <124693949+E-AhrixOne@users.noreply.github.com>
2025-08-05 10:21:20 +0000

    Atualização - Nivel Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,8 @@ int main() {
     float Area, Area2;
     float PIB, PIB2;
     int Pontos_Turisticos, Pontos_Turisticos2;
+    float Densidade_Populacional;
+    float PIB_Per_Capita;
     // Declaração das variáveis 
 
     // Logica de entrada de dados
@@ -72,7 +74,8 @@ int main() {
     printf("Área: %.2f\n", Area);
     printf("PIB: %.2f\n", PIB);
     printf("Número de pontos turísticos: %d\n", Pontos_Turisticos);
-
+    printf("Densidade populacional: %.2f\n", População / Area);
+    printf("PIB per capita: %.2f\n", PIB / População); 
     //
 
     printf("Carta 2:\n");
@@ -83,7 +86,8 @@ int main() {
     printf("Área: %.2f\n", Area2);
     printf("PIB: %.2f\n", PIB2);
     printf("Número de pontos turísticos: %d\n", Pontos_Turisticos2);
-
+    printf("Densidade populacional: %.2f\n", População2 / Area2);
+    printf("PIB per capita: %.2f\n", PIB2 / População2); 
     // comando para finalizar o programa
     return 0;
 }
